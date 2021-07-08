7ee18c24f2229643b2980af302f092b78c1a0acc
Mikael Storman Gustavsson <miga02@gmail.com>
2021-07-08 19:41:42 +0200

    Lists and performance demo

---
+++ ILCode/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ILCode
+{
+  /// <summary>
+  /// This small application serves as a sample for opening IL code.
+  /// We can then compile this code and open it in dnSpy and see how the IL code looks.
+  /// </summary>
+  class Program
+  {
+    public static int Add(int a, int b)
+    {
+      return a + b;
+    }
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
